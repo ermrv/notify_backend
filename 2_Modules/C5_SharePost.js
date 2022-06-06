ca38f5f5d424650f82825bdd8ae6b034d0e7163f
ermrv <eermrv@gmail.com>
2022-06-06 10:51:38 +0530

    sharing post privacy

--- 2_Modules/C5_SharePost.js
+++ 2_Modules/C5_SharePost.js
@@ -6,33 +6,34 @@ const notificationServices = require('../3_SystemKernel/NotificationServices/Not
 exports.sharePost = async (req, res) => {
     try {
         const userId = req.userData.userId;
-        const { postId, channelId, sharedDescription } = req.body;
+        const { postId, channelId, sharedDescription, comments } = req.body;
         if (channelId && postId) {
             const originalPostData = await database.post.findOne({ _id: postId }).lean();
             if (originalPostData.allowSharing == "true") {
-               const postData= await database.post.create({
+                const postData = await database.post.create({
                     shared: "true",
                     postingChannel: channelId,
                     postingUser: userId,
                     sharedDetails: {
-                        originalPostId:postId,
+                        originalPostId: postId,
                         sharedDescription: sharedDescription,
                     },
                     allowCommenting: originalPostData.allowCommenting,
-                    postContent:originalPostData.postContent._id,
+                    postContent: originalPostData.postContent._id,
+                    allowCommenting: comments,
                 });
                 //update channel
                 const channelData = await database.channel.findOneAndUpdate({ _id: channelId }, { $addToSet: { posts: postData._id } }).lean()
                 //add post to channel subscribers
-                await database.user.updateMany({_id:{$in:channelData.subscribers}},{$addToSet:{newsFeedPosts:postData._id}});
-                notificationServices.sendPostNotification({},postData,channelData);
+                await database.user.updateMany({ _id: { $in: channelData.subscribers } }, { $addToSet: { newsFeedPosts: postData._id } });
+                notificationServices.sendPostNotification({}, postData, channelData);
                 return res.status(200).json({ "shared": "true" });
             }
             else {
 
                 return res.status(500).json({ "error": "action not allowed" });
             }
-        }else{
+        } else {
             return res.status(500).json({ "error": "channelId is required" });
         }
     } catch (error) {
