9017d695ba4dbca635f3b795150539bbd9c33896
ermrv <eermrv@gmail.com>
2022-06-06 11:58:21 +0530

    update post privacy

--- 2_Modules/C3_UpdatePostPrivacy.js
+++ 2_Modules/C3_UpdatePostPrivacy.js
@@ -4,20 +4,17 @@ const database = require('./../3_SystemKernel/Database/index')
 exports.updatePostPrivacy = async (req, res) => {
     try {
         const userId = req.userData.userId;
-        const { postId } = req.body;
-        const postData = await database.post.findOne({ _id: postId });
-        if (postData.postingUser == userId) {
-            const { allowCommenting, allowSharing } = req.body;
-                postData.allowCommenting = allowCommenting;
-                postData.allowSharing=allowSharing;
-                await postData.save();
-                res.status(200).json({postData});
+        const { postId,sharing,comments } = req.body;
+        await database.post.updateOne({ _id: postId, postingUser: userId }, {
+            allowCommenting: comments,
+            allowSharing: sharing
+        });
+
+        res.status(200).json({ "updated": true });
 
 
-        }
-        else {
-            return res.status(500).json({ "error": "you are not authorised" });
-        }
+
+
     } catch (error) {
         return res.status(500).json({ error: error.message, });
     }
